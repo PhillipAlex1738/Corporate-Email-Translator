a07e0a66c0d645cffe97e8d848ada8f58d207374
PhillipAlex1738 <interesting5vids@gmail.com>
2025-03-30 17:24:57 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3002,
-    strictPort: true,
-    host: true,
-    watch: {
-      usePolling: true
-    }
-  },
-  preview: {
-    port: 3002,
-    strictPort: true,
-    host: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
-        }
-      }
-    }
+    minify: false // Disable minification to avoid terser dependency
+  },
+  server: {
+    host: true,
+    port: 3000
   }
 })
